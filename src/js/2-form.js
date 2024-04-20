83609639db54104953a107c06a671df893f5af3b
Olesya Lobodko <olesya.lobodko@gmail.com>
2024-04-20 04:56:45 +0200

    2-form done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,30 @@
-console.log('Form');
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
+
+let savedData = JSON.parse(localStorage.getItem(localStorageKey)) ?? {};
+
+form.elements.email.value = savedData.email ? savedData.email.trim() : '';
+form.elements.message.value = savedData.message ? savedData.message.trim() : '';
+
+form.addEventListener('input', event => {
+  savedData.email = event.currentTarget.elements.email.value.trim();
+  savedData.message = event.currentTarget.elements.message.value.trim();
+
+  localStorage.setItem(localStorageKey, JSON.stringify(savedData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const emailForm = event.currentTarget.elements.email.value.trim();
+  const messageForm = event.currentTarget.elements.message.value.trim();
+
+  if (!emailForm || !messageForm) {
+    alert('Please fill all form fields');
+    return;
+  }
+
+  console.log(savedData);
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+});
